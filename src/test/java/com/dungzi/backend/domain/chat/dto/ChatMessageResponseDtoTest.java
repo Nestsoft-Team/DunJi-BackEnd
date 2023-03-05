89b53641ae015308f9fb8ff19bdea1897cd610f6
gyuturn <rbtjrrbals@gmail.com>
2023-03-06 01:01:46 +0900

    [DZ-129] feat&test: stomp헤더로부터 유저 객체 받아오기

--- src/test/java/com/dungzi/backend/domain/chat/dto/ChatMessageResponseDtoTest.java
+++ src/test/java/com/dungzi/backend/domain/chat/dto/ChatMessageResponseDtoTest.java
@@ -6,12 +6,14 @@ import com.dungzi.backend.domain.chat.domain.ChatMessage;
 import com.dungzi.backend.domain.chat.domain.ChatMessageType;
 import com.dungzi.backend.domain.user.domain.User;
 import java.time.LocalDateTime;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ChatMessageResponseDtoTest {
 
 
     @Test
+    @DisplayName("chatMessage를 responseDto로 변경")
     void testToChatMessageResponseDto() {
         //given
         String content="test";
