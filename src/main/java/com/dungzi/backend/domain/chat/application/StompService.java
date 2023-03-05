2ba5ae9f712634c94eeb27814ec4bee0972482ff
gyuturn <rbtjrrbals@gmail.com>
2023-03-06 01:01:34 +0900

    [DZ-129] feat&test: stomp헤더로부터 유저 객체 받아오기

--- src/main/java/com/dungzi/backend/domain/chat/application/StompService.java
+++ src/main/java/com/dungzi/backend/domain/chat/application/StompService.java
@@ -1,20 +1,27 @@
 package com.dungzi.backend.domain.chat.application;
 
 import com.dungzi.backend.domain.user.domain.User;
-import org.springframework.messaging.Message;
+import com.dungzi.backend.global.config.security.jwt.JwtTokenProvider;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.messaging.support.MessageHeaderAccessor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class StompService {
 
-    public User getUserFromHeader(Message message) {
-        StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+    private static final String STOMP_HEADER_ATTR = "simpSessionAttributes";
+    private final JwtTokenProvider jwtTokenProvider;
 
-        UsernamePasswordAuthenticationToken simpleToken = (UsernamePasswordAuthenticationToken) accessor.getHeader(
-                "simpUser");
-        return (User) simpleToken.getPrincipal();
+    public User getUserFromHeader(StompHeaderAccessor accessor) {
+        Map<String, String> simpSessionAttributes = (Map<String, String>) accessor.getHeader(STOMP_HEADER_ATTR);
+        String accessToken = simpSessionAttributes.get(JwtTokenProvider.ACCESS_TOKEN_HEADER_NAME);
+        String refreshToken = simpSessionAttributes.get(JwtTokenProvider.REFRESH_TOKEN_HEADER_NAME);
+
+        Authentication authenticationByToken = jwtTokenProvider.getAuthenticationByToken(accessToken);
+
+        return (User) authenticationByToken.getPrincipal();
     }
 }
